cb8520d090de1f72e42187aeb83754983175fddf
Dhruval Darji <dhruvaldarji@gmail.com>
2018-08-29 16:25:45 -0400

    Random Cowsay Fortune

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # random-cowsay
+
+Print a random message using cowsay, fortune, artii, and lolcat. If no startup mesasge is provided, then "Welcome" will be emitted instead.
+
+## Terminal Headers
+
+Add this line to the bottom of your terminal startup script in order to run a random cowsay fortune whenever the terminal is opened.
+
+## Examples
+
+```bash
+sh random-cowsay/cowscript.sh
+```
+
+```bash
+sh random-cowsay/cowscript.sh "Hello Dhruval"
+```
+
+## Notes
+
+When adding the script to a terminal, make sure to replace the cowsay script with the relative path to the script.
---
+++ cowscript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Retrieve custom welcome text or set to default.
+startupText=${1:-"Welcome"}
+
+# Install necesary packages.
+if ! foobar_loc="$(type -p "artii")" || [[ -z $foobar_loc ]]; then
+    gem install artii
+fi
+if ! foobar_loc="$(type -p "lolcat")" || [[ -z $foobar_loc ]]; then
+    gem install lolcat
+fi
+if ! foobar_loc="$(type -p "fortune")" || [[ -z $foobar_loc ]]; then
+    brew install fortune
+fi
+if ! foobar_loc="$(type -p "cowsay")" || [[ -z $foobar_loc ]]; then
+    brew install cowsay
+fi
+
+# Pick a random cow
+cow=(`cowsay -l | tail -n +2 | tr  " "  "\n" | sort -R | head -n 1`)
+
+# Pick a bubble type between cowsay and cowthink
+declare -a commands=("cowsay" "cowthink")
+rand=$[ $RANDOM % 2 ]
+command=${commands[$rand]}
+
+# Display startup text and cowsay with lolcat.
+(artii $startupText && fortune | $command -f $cow) | lolcat
